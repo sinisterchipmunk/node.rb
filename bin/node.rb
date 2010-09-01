0f2d218662efe2de21e7db273ce462447b96dda5
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2010-09-01 13:19:17 -0400

    made the executable work properly

--- bin/node.rb
+++ bin/node.rb
@@ -1,2 +1,8 @@
 #!/usr/bin/env ruby
-require File.join(File.dirname(__FILE__), "../lib/node-rb")
+require File.join(File.dirname(__FILE__), "../lib/node")
+node = Node.new(*ARGV)
+
+# node.run executes invokes node.js but collects the output; the user will expect
+# the output in stdout when invoking this script so we have to do it this
+# way instead
+system node.command 
