5cccd9d0544d3ea70192f42f7abf3963987f116e
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2010-09-01 11:38:55 -0400

    some cleanup

--- ext/ruby/extconf.rb
+++ ext/ruby/extconf.rb
@@ -39,15 +39,3 @@ ruby_node = Dir[File.join(this_dir, "**/ruby.node")].reject { |s| s =~ /\.svn/ }
 raise "Couldn't find newly-generated ruby.node!" unless ruby_node
 destination = File.join(this_dir, "../..")
 cp(ruby_node, destination)
-
-=begin
-g++ -I. -I/Users/colin/.rvm/rubies/ruby-1.9.2-p0/include/ruby-1.9.1/x86_64-darwin10.4.0 -I/Users/colin/.rvm/rubies/ruby-1.9.2-p0/include/ruby-1.9.1/ruby/backward -I/Users/colin/.rvm/rubies/ruby-1.9.2-p0/include/ruby-1.9.1 -I/Users/colin/projects/gems/node.rb/ext/ruby/ruby -DRUBY_EXTCONF_H=\"extconf.h\" -D_XOPEN_SOURCE -D_DARWIN_C_SOURCE   -fno-common -O3 -ggdb -Wextra -Wno-unused-parameter -Wno-parentheses -Wpointer-arith -Wwrite-strings -Wno-missing-field-initializers -Wshorten-64-to-32 -Wno-long-long  -fno-common -pipe   -o ruby.o -c /Users/colin/projects/gems/node.rb/ext/ruby/ruby/ruby.cpp
-g++ -dynamic -bundle -o ruby.bundle ruby.o -L. -L/Users/colin/.rvm/rubies/ruby-1.9.2-p0/lib -L. -L/usr/local/lib
-    -Wl,-undefined,dynamic_lookup -Wl,-multiply_defined,suppress -Wl,-flat_namespace  -lruby.1.9.1  -lpthread -ldl
-    -lobjc  -lstdc++
-# '/usr/bin/g++', '-g', '-fPIC', '-compatibility_version', '1', '-current_version', '1', '-DEV_MULTIPLICITY=0',
-   '-I/Users/colin/.rvm/rubies/ruby-1.9.2-p0/include/ruby-1.9.1', '-I/usr/local/include/node', '../ruby19.cpp', '-c',
-   '-o', 'default/ruby19_2.o'
-cd -
-=end
-
